9ccf31f35f88173f82ca4c73bc09916151319e53
Aphile048mazibuko <St10518270@rcconnect.edu.za>
2026-08-24 20:41:34 +0200

    Add files via upload

---
+++ Admission_System.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.admission_system;
+
+/**
+ *
+ * @author mnqob
+ */
+public class Admission_System {
+    //Allow staff members to manage patient records
+    //---------store patient records----------
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
